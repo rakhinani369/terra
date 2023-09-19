bfb1b7e3eabaefa62cff8bb5803d5231e862214a
nani <rakeshpeddapally369@gmail.com>
2023-09-19 13:04:03 -0700

    1st commit

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ secret_key = "zEdUVNduT6bLwmh/HiaKZRuNg8SWui92R7h8rYzf"
 
 resource "aws_instance" "example" {
 ami ="ami-0f8e81a3da6e2510a"
-instance_type = "t2.micro"
+instance_type = "t2.large"
 key_name      = "kube8"
 tags          = {
 Name	      = "sandy"
